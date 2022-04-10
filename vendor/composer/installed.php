492f93953ec75a82f7758fbb4ae94ebfb8d598d2
Maria Katrina Ocampo <ocampo.mkb@gmail.com>
2022-04-10 11:36:44 -0300

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '06091cb5fb89150cb9555959ff8a7be6480d1892',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '06091cb5fb89150cb9555959ff8a7be6480d1892',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v215',
+            'version' => '215.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => '0c1185ad87156242eeadaae1d7fa10c07689b173',
+            'dev_requirement' => true,
+        ),
+    ),
+);
